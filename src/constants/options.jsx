9d760505390d5f86d9e0179663ed882f77eac0a9
kaviyasrikj <146372727+kaviyasrikj@users.noreply.github.com>
2025-01-30 15:48:09 +0530

    Third Commit

--- src/constants/options.jsx
+++ src/constants/options.jsx
@@ -3,6 +3,51 @@ export const SelectTravelesList=[
         id:1,
         title:'Just Me',
         desc:'Asole traveles in exploration',
-        icon:''
-    }
+        icon:'🛩️',
+        people:'1'
+    },
+    { 
+        id:2,
+        title:'A Couple',
+        desc:'Two traveles in tandem',
+        icon:'🍻',
+        people:'2 People'
+    },
+    { 
+        id:3,
+        title:'Family',
+        desc:'A group of fun loving adv',
+        icon:'👨🏻‍👩🏻‍👧🏻‍👧🏻',
+        people:'3 to 5 People'
+    },
+    { 
+        id:4,
+        title:'Friends',
+        desc:'A bunch of thrill-seekes',
+        icon:'🚢',
+        people:'5 to 10 People'
+    },
 ]
+
+export const SelectBudgetOptions=[
+    { 
+        id:1,
+        title:'Cheap',
+        desc:'Stay conscious of costs',
+        icon:'🪙',
+    },
+    { 
+        id:2,
+        title:'Moderate',
+        desc:'Keep cost on the average side',
+        icon:'💰',
+    },
+    { 
+        id:3,
+        title:'Luxury',
+        desc:'Dont worry about cost',
+        icon:'💸',
+    },
+]
+
+export const AI_PROMPT='Generate Travel Plan for Location : {location}'
